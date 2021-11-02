293ddd97c06eaee2c40f97dc231ef146dc9b5edd
Kim Jin-yeong <jinzero29@gmail.com>
2021-11-02 09:51:14 +0900

    amend time.sleep

--- fetch.py
+++ fetch.py
@@ -92,7 +92,7 @@ def fetch_info():  # get information from NEIS Service
             print(f"sql executed")
 
         school_db.commit()
-        time.sleep(1.5)
+        time.sleep(2)
 
     school_db.close()
 
